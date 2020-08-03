ea20c4deac433272b1258597aabe39e599505d67
Kirill Matskevich <inflamedbrain@gmail.com>
2020-08-03 15:12:51 +0300

    renamed class name for avoiding conflict with SEGAnalytics reachability class

--- Sources/Reachability.swift
+++ Sources/Reachability.swift
@@ -43,10 +43,10 @@ public extension Notification.Name {
     static let reachabilityChanged = Notification.Name("reachabilityChanged")
 }
 
-public class Reachability {
+public class ReachabilitySwift {
 
-    public typealias NetworkReachable = (Reachability) -> ()
-    public typealias NetworkUnreachable = (Reachability) -> ()
+    public typealias NetworkReachable = (ReachabilitySwift) -> ()
+    public typealias NetworkUnreachable = (ReachabilitySwift) -> ()
 
     @available(*, unavailable, renamed: "Connection")
     public enum NetworkStatus: CustomStringConvertible {
@@ -167,7 +167,7 @@ public class Reachability {
     }
 }
 
-public extension Reachability {
+public extension ReachabilitySwift {
 
     // MARK: - *** Notifier methods ***
     func startNotifier() throws {
@@ -253,7 +253,7 @@ public extension Reachability {
     }
 }
 
-fileprivate extension Reachability {
+fileprivate extension ReachabilitySwift {
 
     func setReachabilityFlags() throws {
         try reachabilitySerialQueue.sync { [unowned self] in
@@ -282,7 +282,7 @@ fileprivate extension Reachability {
 
 extension SCNetworkReachabilityFlags {
 
-    typealias Connection = Reachability.Connection
+    typealias Connection = ReachabilitySwift.Connection
 
     var connection: Connection {
         guard isReachableFlagSet else { return .unavailable }
@@ -399,8 +399,8 @@ extension SCNetworkReachabilityFlags {
  - still allow for automatic stopping of the notifier on `deinit`.
  */
 private class ReachabilityWeakifier {
-    weak var reachability: Reachability?
-    init(reachability: Reachability) {
+    weak var reachability: ReachabilitySwift?
+    init(reachability: ReachabilitySwift) {
         self.reachability = reachability
     }
 }
